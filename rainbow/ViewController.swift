7dfa294a3d501cb467e594ffa7a14644e8d54191
Griffin Thompson <24gthompson@go.dsdmail.net>
2021-05-19 09:37:25 -0600

    Initial Commit

---
+++ rainbow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rainbow
+//
+//  Created by Student on 5/19/21.
+//  Copyright © 2021 Sunset Junior Highschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
